489277d8a9abfae5e7ab1914bb34e96bb23ac786
Gammarkin <you@example.com>
2022-11-11 15:48:43 -0300

    fix: typo

--- src/services/Character.ts
+++ src/services/Character.ts
@@ -3,33 +3,33 @@ import {IModel} from '../interfaces/IModel';
 import ICharacter from '../interfaces/IChar';
 
 class CharacterService implements IService<ICharacter> {
-	private _car: IModel<ICharacter>;
+	private _char: IModel<ICharacter>;
 
 	constructor(model: IModel<ICharacter>) {
-		this._car = model;
+		this._char = model;
 	}
 
 	public async create(obj: ICharacter): Promise<ICharacter> {
-		return this._car.create(obj);
+		return this._char.create(obj);
 	}
 
 	public async findAll(): Promise<ICharacter[]> {
-		return this._car.read();
+		return this._char.read();
 	}
 
 	public async findById(id: string): Promise<ICharacter | null> {
-		return this._car.readOne(id);
+		return this._char.readOne(id);
 	}
 
 	public async updateOne(
 		id: string,
 		obj: ICharacter
 	): Promise<ICharacter | null> {
-		return this._car.update(id, obj);
+		return this._char.update(id, obj);
 	}
 
 	public async destroy(id: string): Promise<ICharacter | null> {
-		return this._car.delete(id);
+		return this._char.delete(id);
 	}
 }
 
