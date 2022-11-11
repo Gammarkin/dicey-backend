5dff8bd83d6a655599620e2bb36d72638c0ece6b
Gammarkin <you@example.com>
2022-11-11 19:29:22 -0300

    refactor: changed the way to see desired user

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -12,22 +12,26 @@ abstract class MongoModel<T> implements IModel<T> {
 		return this._model.create({...obj});
 	}
 
-	public async readOne(id: string): Promise<T | null> {
-		return this._model.findOne({id});
+	public async readOne(playerTag: string): Promise<T | null> {
+		return this._model.findOne({playerTag});
 	}
 
 	public async read(): Promise<T[]> {
 		return this._model.find({});
 	}
 
-	public async update(id: string, obj: Partial<T>): Promise<T | null> {
-		return this._model.findOneAndUpdate({id}, {...obj} as UpdateQuery<T>, {
-			new: true,
-		});
+	public async update(playerTag: string, obj: Partial<T>): Promise<T | null> {
+		return this._model.findOneAndUpdate(
+			{playerTag},
+			{...obj} as UpdateQuery<T>,
+			{
+				new: true,
+			}
+		);
 	}
 
-	public async delete(id: string): Promise<void> {
-		await this._model.deleteOne({id});
+	public async delete(playerTag: string): Promise<void> {
+		await this._model.deleteOne({playerTag});
 	}
 }
 
