e7aa0624ecf204c976360ab7c8819a24546a8d96
Gammarkin <you@example.com>
2022-11-12 17:46:49 -0300

    refactor:  route

--- src/routes/character.ts
+++ src/routes/character.ts
@@ -1,12 +1,22 @@
 import {Router} from 'express';
+
 import CharacterController from '../controllers/Character';
+import CharacterService from '../services/Character';
+import CharacterModel from '../models/Character';
 
 const router = Router();
 
-router.get('/', CharacterController.get);
-router.get('/:id', CharacterController.getOne);
-router.post('/', CharacterController.post);
-router.put('/:id', CharacterController.put);
-router.delete('/:id', CharacterController.remove);
+const charModel = new CharacterModel();
+const charService = new CharacterService(charModel);
+const charController = new CharacterController(charService);
+
+router.get('/', (req, res) => charController.findAll(req, res));
+router.get('/:playerTag', (req, res) =>
+	charController.findByPlayerTag(req, res)
+);
+
+router.post('/', (req, res) => charController.create(req, res));
+router.put('/:playerTag', (req, res) => charController.update(req, res));
+router.delete('/:playerTag', (req, res) => charController.destroy(req, res));
 
 export default router;
